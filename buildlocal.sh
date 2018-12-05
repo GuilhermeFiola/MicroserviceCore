9b102b99bb7dbd970d55ba1861b71c39150c599b
Guilherme Fiola <guilherme.fiola@consinco.com.br>
2018-12-05 15:42:23 -0200

    Alteração do build local

--- buildlocal.sh
+++ buildlocal.sh
@@ -1,3 +1,3 @@
 rm -rf _builds _steps _projects _cache _temp
-wercker build --git-domain github.com --git-owner GuilhermeFiola --git-repository MicroserviceCore
+wercker build --git-domain github.com --git-owner GuilhermeFiola --git-repository TeamService
 rm -rf _builds _steps _projects _cache _temp
